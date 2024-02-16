0f507b5b4a30cfa150d4d3f6572b5b668a457c29
LakshyaSvt <142570590+LakshyaSvt@users.noreply.github.com>
2024-02-16 15:31:05 +0530

    admin notify

--- codes/resources/js/app.js
+++ codes/resources/js/app.js
@@ -1,5 +1,6 @@
 import adminRouter from "./Router/adminRouter";
 import Vue from 'vue'
+import Pusher from 'pusher-js';
 import VueRouter from 'vue-router'
 import Multiselect from 'vue-multiselect'
 import "vue-multiselect/dist/vue-multiselect.min.css"
@@ -14,6 +15,7 @@ Vue.use(VueRouter);
 var $ = global.jQuery;
 window.$ = $;
 
+Vue.component('main-app', require('@pages/admin/Main.vue').default);
 //main-components
 Vue.component('side-bar', require('@components/SideBar.vue').default);
 Vue.component('navbar', require('@components/NavBar.vue').default);
@@ -27,6 +29,12 @@ Vue.component('ImageModal', require('@components/ImageModal.vue').default);
 //lib-components
 Vue.component('RichTextEditor', require('@components/RichTextEditor.vue').default);
 Vue.component('Multiselect', Multiselect)
+
+//Pusher Config
+window.Pusher = new Pusher(process.env.MIX_PUSHER_APP_KEY, {
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    encrypted: true
+});
 
 const app = new Vue({
     el: '#app',
