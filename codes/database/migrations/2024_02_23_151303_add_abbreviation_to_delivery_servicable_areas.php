3a355a9b6c264d3d38b2186f7b7141215ac9a471
LakshyaSvt <142570590+LakshyaSvt@users.noreply.github.com>
2024-02-23 15:13:43 +0530

    migration

--- codes/database/migrations/2024_02_23_151303_add_abbreviation_to_delivery_servicable_areas.php
+++ codes/database/migrations/2024_02_23_151303_add_abbreviation_to_delivery_servicable_areas.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAbbreviationToVendorPayments extends Migration
+class AddAbbreviationToDeliveryServicableAreas extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddAbbreviationToVendorPayments extends Migration
      */
     public function up()
     {
-        Schema::table('vendor_payments', function (Blueprint $table) {
+        Schema::table('delivery_servicable_areas', function (Blueprint $table) {
             $table->string('abbreviation')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddAbbreviationToVendorPayments extends Migration
      */
     public function down()
     {
-        Schema::table('vendor_payments', function (Blueprint $table) {
+        Schema::table('delivery_servicable_areas', function (Blueprint $table) {
             $table->dropColumn('abbreviation');
         });
     }
